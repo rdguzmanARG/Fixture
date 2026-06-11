96820f0f9863a7c4a15a4cc25277b44533fa920b
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2026-06-11 18:24:13 -0300

    Add mysql configuration

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,5 +1,7 @@
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
