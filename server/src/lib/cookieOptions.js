ae2cb1424ab6160334f9374d92391927a1784c5e
Ricardo D. de Guzmán <rdguzman@tercerplaneta.com>
2026-06-12 12:22:03 -0300

    remove https

--- server/src/lib/cookieOptions.js
+++ server/src/lib/cookieOptions.js
@@ -1,8 +1,6 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
 export const cookieOptions = {
   httpOnly: true,
   sameSite: 'lax',
-  secure: isProduction,
+  secure: false,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
